0c1b276054b04165e4d0863ab4a91db7d29ec1a1
Isabella Luiza Souza <79116590+isabluiza@users.noreply.github.com>
2022-06-11 14:01:22 -0300

    Create maior-menos.cpp

---
+++ manzano/maior-menos.cpp
@@ -0,0 +1,35 @@
+/*Efetuar a leitura de dois números inteiros e identificar o maior e o menor valores.*/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    float numero, maior, menor;
+    int cont = 1;
+    
+        while (cont <= 2) {
+            
+        printf("Digite um número real:\n");
+        
+        scanf("%f", &numero);
+           
+            if (cont == 1) {
+                maior = numero;
+                menor = maior;
+            }
+                else if (numero > maior) {
+                    maior = numero;
+                }
+                    else if (numero < menor) {
+                        menor = numero;
+                    }
+        
+            cont++;
+        }
+        
+    printf("Menor numero: %.2f\nMaior numero: %.2f", menor, maior);
+    
+    return(0);
+}
