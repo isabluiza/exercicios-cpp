b702b958c2e8e15107b02ed3b2eb29ae60e9e759
Isabella Luiza Souza <79116590+isabluiza@users.noreply.github.com>
2022-06-12 10:10:41 -0300

    Add files via upload

---
+++ outros/indice.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+    int idade[4], indice;
+
+    for (indice=0; indice<4; indice++) {
+
+        printf("Digite um valor:");
+        scanf("%d", &idade[indice]);
+    }
+
+    for (indice=0; indice<4; indice++) {
+
+        printf("\nValor digitado: %d", idade[indice]);
+    }
+
+
+return 0;
+}
---
+++ outros/matriz.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int matriz[2][3];
+	
+	int linha, coluna;
+	
+		for (linha=0; linha < 1; linha++) {
+			
+			for (coluna=0; coluna < 3; coluna++){
+				
+				printf("Digite os valores:");
+				scanf("%d", &matriz[linha][coluna]);
+				
+			}
+		}
+		
+		do {
+				
+			for (coluna=0; coluna < 3; coluna++){
+				
+				matriz[1][coluna] * 2;
+				printf("\nA multiplicacao e %d", matriz[coluna]);
+		
+			}
+			
+		} while (linha=2);
+		
+		
+return 0;
+}
+
